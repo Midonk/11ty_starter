64e8ebb03bbe86aca9eb69e60fc0ffe09042b242
Midonk <t_finet@hotmail.fr>
2019-11-14 11:38:13 +0100

    add all files

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("src/assets/img/");
+
+    return {
+        dir: {
+            input: "src",
+            output: "dist"
+        }
+    };
+};
